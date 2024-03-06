6acc1d84e9afb2ce09c98d4caafb52dd0d402e52
Aluno <Aluno@ETEJK-LAB03-M07>
2024-03-05 21:44:16 -0300

    criação repositorio e entidade do setor e docente.

---
+++ horario/src/main/java/br/com/horario/entity/SetorEntity.java
@@ -0,0 +1,36 @@
+package br.com.horario.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "setor", schema = "hoarario")
+public class SetorEntity  implements Serializable {
+
+	private static final long SerialVersionUID = 1L;
+			@Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+	        @Column (name = "id_setor")
+			private Long idSetor;
+			@Column(name = "nome")
+			private String nome;
+			public Long getIdSetor() {
+				return idSetor;
+			}
+			public void setIdSetor(Long idSetor) {
+				this.idSetor = idSetor;
+			}
+			public String getNome() {
+				return nome;
+			}
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+}
+
