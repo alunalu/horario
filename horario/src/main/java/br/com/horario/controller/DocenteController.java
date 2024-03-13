5e134f9d78d2be242873d9891a43ad921bec88bb
Aluno <Aluno@ETEJK-LAB03-M07>
2024-03-12 21:48:43 -0300

    criação dos serviços de docentes e setores

--- horario/src/main/java/br/com/horario/controller/DocenteController.java
+++ horario/src/main/java/br/com/horario/controller/DocenteController.java
@@ -1,13 +1,26 @@
 package br.com.horario.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.horario.service.DocenteService;
+import br.com.horario.service.SetorService;
 
 @Controller
 public class DocenteController {
+	
+	@Autowired
+	private DocenteService docenteService;
+	@Autowired
+	private SetorService setorService;
+	
    @GetMapping("/docente") // nome que eu quiser colocar
-   public String docente ()
+   public String docente (ModelMap model)
    {
+	   model.addAttribute("docente",docenteService.findAnll());
+	   model.addAttribute("setores",setorService.finAll());
 	   return "docente"; //caminho real do arquivo 
    }
 }
